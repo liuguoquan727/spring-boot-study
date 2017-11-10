da1d313693c25b5c8fba82a4b1536ef225f56b80
刘国权 <liuguoquan727@126.com>
2017-11-10 15:23:16 +0800

    [feature]-jdbc

--- springboot-mysql-jdbc/src/main/java/com/liuguoquan/springboot/JDBCApplication.java
+++ springboot-mysql-jdbc/src/main/java/com/liuguoquan/springboot/JDBCApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class JDBCApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(JDBCApplication.class, args);
+  }
 }
