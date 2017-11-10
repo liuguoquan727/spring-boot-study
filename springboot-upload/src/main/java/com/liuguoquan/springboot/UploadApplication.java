2cf0c5942de7fdbf535673a771ac2e23a82547cb
刘国权 <liuguoquan727@126.com>
2017-11-10 15:45:48 +0800

    [feature]-springboot 文件上传

--- springboot-upload/src/main/java/com/liuguoquan/springboot/UploadApplication.java
+++ springboot-upload/src/main/java/com/liuguoquan/springboot/UploadApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class UploadApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+		SpringApplication.run(UploadApplication.class, args);
 	}
 }
